4b5228dffa3078957037fa295e5826fada0e5386
Jose <jevarelaz@gmail.com>
2021-09-05 01:48:27 -0500

    make it better

---
+++ controllers/usuarios.js
@@ -0,0 +1,78 @@
+const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
+
+
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page, 
+        limit
+    });
+}
+
+const usuariosPost = async (req, res = response) => {
+
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
+
+    //verificar si el correo existe
+
+    const existeEmail = await Usuario.findOne({ correo });
+    if ( existeEmail ) {
+        return res.status(400).json({
+            msg: 'Ese correo ya esta registrado'
+        });
+    }
+    //encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password,salt);
+
+    //guarda en BD
+
+    await usuario.save(); 
+
+    res.json({
+        usuario
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - usuariosPut',
+        id
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    });
+}
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete,
+}
